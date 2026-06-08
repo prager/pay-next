8f9703a08c776df66cf5badad8146a1c8db71f16
prager <jkulisek.us@gmail.com>
2026-06-08 07:03:04 -0700

    Converted to CI4 - no Stripe yet

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -58,7 +58,9 @@ class Autoload extends AutoloadConfig
      *
      * @var array<string, string>
      */
-    public $classmap = [];
+    public $classmap = [
+        APPPATH . 'ThirdParty/stripe-php/lib',
+    ];
 
     /**
      * -------------------------------------------------------------------
